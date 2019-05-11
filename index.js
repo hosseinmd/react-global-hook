f7581c829a54fb2771319da831329f169cffd0e2
hossein mohammadi <hosseinm.developer@gmail.com>
2019-05-11 17:16:08 +0430

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+function setState(newState = {}) {
+  this.state = { ...this.state, ...newState };
+  let queueUpdate = [];
+  Object.keys(newState).forEach(key => {
+    if (this.listeners[key])
+      this.listeners[key].forEach(listener => {
+        if (!queueUpdate.includes(listener)) queueUpdate.push(listener);
+      });
+  });
+  queueUpdate.forEach(listener => {
+    listener(this.state);
+  });
+}
+
+function useGlobal(sensitiveStateKeys, listener) {
+  if (typeof listener !== "function") listener = React.useState()[1];
+  if (!Array.isArray(sensitiveStateKeys))
+    sensitiveStateKeys = Object.keys(this.listeners);
+  React.useEffect(() => {
+    sensitiveStateKeys.forEach(stateKey => {
+      this.listeners[stateKey] = [
+        ...(this.listeners[stateKey] || []),
+        listener
+      ];
+    });
+    return () => {
+      sensitiveStateKeys.forEach(stateKey => {
+        this.listeners[stateKey] = this.listeners[stateKey].filter(
+          prevListener => prevListener !== listener
+        );
+      });
+    };
+  }, []);
+  return [this.state, this.actions];
+}
+
+function associateActions(store, actions = {}) {
+  const associatedActions = {};
+  Object.keys(actions).forEach(key => {
+    if (typeof actions[key] === "function") {
+      associatedActions[key] = actions[key].bind(null, store);
+    }
+    if (typeof actions[key] === "object") {
+      associatedActions[key] = associateActions(store, actions[key]);
+    }
+  });
+  return associatedActions;
+}
+
+export const createState = (initialState, actions, initializer) => {
+  const store = { state: initialState, listeners: {} };
+  store.setState = setState.bind(store);
+  store.actions = associateActions(store, actions);
+  if (initializer) initializer(store);
+  return {
+    useGlobal: useGlobal.bind(store),
+    getGlobal() {
+      return [store.state, store.actions];
+    }
+  };
+};
+
---
+++ readme.md
@@ -0,0 +1,73 @@
+![alt text](https://raw.githubusercontent.com/hosseinmd/react-global-hook/master/react-global-hook.png)
+
+[![NPM](https://nodei.co/npm/react-global-hook.png)](https://nodei.co/npm/react-global-hook/)
+
+[![install size](https://packagephobia.now.sh/badge?p=react-global-hook)](https://packagephobia.now.sh/result?p=react-global-hook) [![dependencies](https://david-dm.org/hosseinmd/react-global-hook.svg)](https://david-dm.org/hosseinmd/react-global-hook.svg)
+
+# react-global-hook
+
+Easy state management for react & react-native using hooks.
+
+## install
+
+```npm
+npm i react-global-hook --save
+```
+
+---
+
+### use
+
+```javascript
+import React from "react";
+import { createState } from "react-global-hook";
+
+const initialState = {
+  counter: 0
+};
+
+const actions = {
+  addToCounter: (store, amount) => {
+    const newCounterValue = store.state.counter + amount;
+    store.setState({ counter: newCounterValue });
+  }
+};
+
+const { useGlobal, getGlobal } = createState(React, initialState, actions);
+
+const App = () => {
+  const [globalState, globalActions] = useGlobal(["counter"]);
+  // this component update just when `counter` did update
+  // if useGlobal parameter isn't defined this function will be update at any change state
+  // if parameter is empty array like [] this function will never be update
+  // if 2nd parameter define function this function will be run instead update componentF
+  return (
+    <div>
+      <p>
+        counter:
+        {globalState.counter}
+      </p>
+      <button type="button" onClick={() => globalActions.addToCounter(1)}>
+        +1 to global
+      </button>
+    </div>
+  );
+};
+
+export default App;
+```
+
+### getGlobal
+
+this function is for use state and actions out of component
+
+```javascript
+function myApi(){
+  ...
+  const [globalState, globalActions] = getGlobal();
+  ...
+}
+
+```
+
+---
