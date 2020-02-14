6430f15359adc68a7bd9f38c89e17a2b6d63e2f1
Hossein Mohammadi <hosseinm.developer@gmail.com>
2020-02-14 13:59:54 +0330

    delete deprecated create state

--- src/index.js
+++ src/index.js
@@ -3,18 +3,4 @@
 import { createStore } from "./core";
 import { createHooks, useLocalStore } from "./hooks";
 
-/**
- * @deprecated Please use createStore
- * @template S , A
- * @type {function}
- * @param {S} initialState
- * @param {A} actions
- * @param {(store: import("./core").Store<S, A>) => void} initializer
- * @returns {[ (sensitiveStateKeys: (keyof S)[], listener: (S) => void) => [S, A],() => [S, A] ]}
- */
-export function createState(initialState, actions, initializer) {
-  const store = createStore(initialState, actions, initializer);
-  return [createHooks(store), () => [store.state, store.actions], store];
-}
-
 export { createStore, createHooks, useLocalStore };
